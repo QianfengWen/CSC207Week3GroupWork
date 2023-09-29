335bd1f8bc7f04e3c6b3be4019db87425df9c405
egoist-hozumi <taowen.qian@mail.utoronto.ca>
2023-09-29 18:08:50 -0400

    Clean no need codes.

--- CSC207Week3GroupWork/src/test/java/ApiCaller.java
+++ CSC207Week3GroupWork/src/test/java/ApiCaller.java
@@ -1,5 +1,4 @@
-import example.OkHttpClient;
-import org.json.JSONObject;
+import api.OkHttpClient;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -10,10 +9,6 @@ public class  ApiCaller {
         OkHttpClient client = new OkHttpClient();
         String run = client.run("http://api.airvisual.com/v2/nearest_city?key=479c3350-e1bb-4911-96c9-fec8fb2555f6");
         System.out.println(run);
-//        JSONObject jsonObject = new JSONObject(run);
-//        System.out.println(jsonObject);
-//        HashMap<String, String> content = jsonObject.getMapType("data");
-//        System.out.println(content);
     }
 }
 
